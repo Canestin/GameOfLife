adfa38e12f1b60f365339f4097a9f5d2e9f428e8
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-04-16 00:50:59 +0200

    chore

--- src/GOL/Cell.java
+++ src/GOL/Cell.java
@@ -4,21 +4,22 @@ public class Cell {
     public int state;
     public int livingNeighbors;
 
-    public Cell(int state, int livingNeighbors){
+    public Cell(int state, int livingNeighbors) {
         this.state = state;
         this.livingNeighbors = livingNeighbors;
     }
 
     /**
      * Given an initial state for the cell, computes its next state
+     * 
      * @return
      */
-    public int NextState(){
-        if(state == CellState.ALIVE && (livingNeighbors == 2 || livingNeighbors == 3)) {
+    public int NextState() {
+        if (state == CellState.ALIVE && (livingNeighbors == 2 || livingNeighbors == 3)) {
             return CellState.ALIVE;
         }
 
-        if(state == CellState.DEAD && livingNeighbors == 3) {
+        if (state == CellState.DEAD && livingNeighbors == 3) {
             return CellState.ALIVE;
         }
 
